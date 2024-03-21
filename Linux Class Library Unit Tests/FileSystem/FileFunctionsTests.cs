ca13cdef21b05962b74ece43a348148d150c099d
SKYLINE2\MichielSA <michiel.saelen@skyline.be>
2024-03-21 17:22:55 +0800

    remove empty test

--- Linux Class Library Unit Tests/FileSystem/FileFunctionsTests.cs
+++ Linux Class Library Unit Tests/FileSystem/FileFunctionsTests.cs
@@ -41,11 +41,5 @@ namespace Skyline.DataMiner.Utils.Linux.FileSystem.Tests
 			Assert.AreEqual("/etc/cassandra/cassandra.yaml", file.Path, "Path");
 			Assert.AreEqual("cassandra.yaml", file.Name, "Name");
 		}
-
-		[TestMethod()]
-		public void GetFileTestSpaces() 
-		{
-			// TODO
-		}
 	}
 }
